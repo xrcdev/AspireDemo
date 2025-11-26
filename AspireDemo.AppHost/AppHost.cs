f4e1179046de8a41cffaf8ec6298c7da174e4513
xuruichao <895167799@qq.com>
2025-11-26 18:42:13 +0800

    9.5#
    支持 Kubernetes 和 Helm Chart 部署
    
    更新项目文件以集成 Docker 和 Kubernetes 支持，添加 Kubernetes 环境配置和 Redis 缓存更新。新增 Kubernetes 配置文件和 Helm Chart 文件，包括 `aspire-manifest.json`、`Chart.yaml`、`values.yaml` 等，以定义应用程序的资源、部署、服务和机密配置。这些更改使项目能够在 Kubernetes 环境中运行，并通过 Helm Chart 进行管理和部署。

--- AspireDemo.AppHost/AppHost.cs
+++ AspireDemo.AppHost/AppHost.cs
@@ -2,10 +2,19 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+// 添加 Kubernetes 环境配置
+builder.AddKubernetesEnvironment("k8s")
+    .WithProperties(k8s =>
+    {
+        k8s.HelmChartName = "aspiredemo"; // Helm chart 名称
+        k8s.DefaultImagePullPolicy = "IfNotPresent"; // 镜像拉取策略
+    });
+var cache = builder.AddRedis("cache")
+ .PublishAsConnectionString(); // 添加发布支持
+
 
 var apiService = builder.AddProject<Projects.AspireDemo_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health").WithReplicas(2);
+    .WithHttpHealthCheck("/health").WithReplicas(2).PublishAsDockerFile(); // 生成 Dockerfile;
 
 builder.AddProject<Projects.AspireDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
@@ -13,7 +22,7 @@ builder.AddProject<Projects.AspireDemo_Web>("webfrontend")
     .WithReference(cache)
     .WaitFor(cache)
     .WithReference(apiService)
-    .WaitFor(apiService);
+    .WaitFor(apiService).PublishAsDockerFile();
 
 //var scalar= builder.AddScalarApiReference();
 //scalar.WithReference(apiService);
